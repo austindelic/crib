f06ea1c9d30ad151492f3e878ddc21399f3c6c7c
Austin Delic <delicaustin0@gmail.com>
2025-05-03 16:55:01 +0800

    Added new migration files, updated schema references, and refined configurations

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # sv
 
+VISIT: [crib-peach.vercel.app](https://crib-peach.vercel.app/)
+
 Crib is a house management app built by hackers, for hackers. Designed for shared hacker houses, Crib smooths out the chaos of eight people in a two-bedroom apartment. From voting on issues and sharing one-time secrets to sending affirmations and tracking chores — all with Discord integration — Crib brings it all together in one sleek, collaborative dashboard.
 
 HackClub Neigherbpurhood app idea: Dormroom big brother helper. log complains with an upvote downvote system, a built in app to determine what time people are free, a link to share sensative info (1 time instant link), an affirmation system. An internal member vote system to determine leader (chess elo system based on voting)
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,33 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, text, uuid, timestamp, integer } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
+const baseFields = {
+	id: uuid('id').defaultRandom().primaryKey(),
+	createdAt: timestamp('created_at').defaultNow()
+};
+
+export const users = pgTable('users', {
+	...baseFields,
+
+	username: text('username').notNull(),
+	email: text('email').notNull(),
+	age: integer('age'),
+	name: text('name'),
+	discordId: text('discord_id')
+});
+
+export const houses = pgTable('houses', {
+	...baseFields,
+
+	name: text('name').notNull()
+});
+
+export const house_users = pgTable('house_users', {
+	...baseFields,
+
+	user_id: uuid('user_id')
+		.references(() => users.id)
+		.notNull(),
+	house_id: uuid('house_id')
+		.references(() => houses.id)
+		.notNull()
 });
