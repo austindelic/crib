b620fc900ca6ecb06b6243fb575a51969af6fbb7
Austin Delic <delicaustin0@gmail.com>
2025-05-21 21:18:44 +0800

    refactor: remove unused onboarding modal and app page components bun check = return 0

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,10 +1,10 @@
 import type { PageServerLoad } from './$types';
-import { getHouseUserFromUserId } from '$server/db/queries/house_users';
+import { getHousesFromUserId } from '$server/db/queries/house_users';
 import type { User } from '$schema_types';
 
 export const load: PageServerLoad = async (event) => {
 	const user = event.locals.user as User;
-	const houses = await getHouseUserFromUserId(user.id);
+	const houses = await getHousesFromUserId(user.id);
 	return {
 		user,
 		houses
