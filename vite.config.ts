5d209e8017bd2a32b7f5eee562acbf3c189b0d1b
Austin Delic <delicaustin0@gmail.com>
2025-05-23 17:45:56 +0800

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { threlteStudio } from '@threlte/studio/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [threlteStudio(), tailwindcss(), sveltekit()]
 });
