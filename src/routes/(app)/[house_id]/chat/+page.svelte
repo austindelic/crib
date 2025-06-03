99ae8a6399e294888604716618bb692e83db111c
Austin Delic <delicaustin0@gmail.com>
2025-06-03 23:56:44 +0800

    now editor for chat input and more cleanup.

--- src/routes/(app)/[house_id]/chat/+page.svelte
+++ src/routes/(app)/[house_id]/chat/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
 	import type { House, HouseChat, User } from '$schema_types';
-	import { Input, Button, Card } from 'flowbite-svelte';
+	import { Button, Card } from 'flowbite-svelte';
 	import MarkdownRenderer from '$ui/components/MarkdownRenderer.svelte';
 	import { enhance } from '$app/forms';
 	import { supabase } from '$utils/supabase_client.utils.js';
 	import { invalidateAll } from '$app/navigation';
 	import { browser } from '$app/environment';
+	import MarkdownEditor from '$ui/components/MarkDownEditor.svelte';
+	import { onDestroy } from 'svelte';
+
 	const { data } = $props();
 	const { user, house }: { user: User; house: House } = data;
 	const { chats }: { chats: HouseChat[] } = $derived(data);
-	let chat_input = $state('');
+	let { chat_input }: { chat_input: string } = $derived(data);
 	const house_chat_channel = supabase.channel(house.id);
 	house_chat_channel
 		.on('broadcast', { event: 'new_chat' }, () => {
@@ -18,6 +21,12 @@
 			}
 		})
 		.subscribe();
+
+	onDestroy(() => {
+		if (house_chat_channel) {
+			house_chat_channel.unsubscribe();
+		}
+	});
 </script>
 
 <div
@@ -47,13 +56,11 @@
 	</main>
 
 	<footer class="flex items-center gap-2 border-t border-gray-300 p-4 dark:border-neutral-700">
-		<form method="POST" use:enhance action="?/send">
-			<Input
-				placeholder="Type your message..."
-				class="flex-1"
-				name="chat"
-				bind:value={chat_input}
-			/>
+		<form method="POST" use:enhance action="?/send" class="flex w-full gap-2">
+			<div class="flex-1">
+				<MarkdownEditor class="w-full" bind:value={chat_input} />
+			</div>
+			<input type="hidden" name="chat" bind:value={chat_input} />
 			<Button type="submit" color="blue">Send</Button>
 		</form>
 	</footer>
